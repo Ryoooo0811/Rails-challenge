31b0ae87a355ea3c1f56a69bd8cc9258fb1d5a45
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-25 14:45:18 +0000

    Book index finish,console error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction)
+    params.require(:user).permit(:name, :profile_image_id, :introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'about' => 'homes#about'
   resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
