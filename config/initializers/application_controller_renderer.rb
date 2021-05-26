23bbdb12c86cd76d9ae449ad56c89f5a78d7c88c
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-26 14:43:42 +0000

    validation [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '437f64778b0036d790ecab1673292b01f3a73fe2cb125b84c2f020c470413323ca14eb2d668bbe55e2e8bdc4eea7fe93d188361d6e5c2ece4b7f96d33c4737f1'
