93729537509d8e35c0892c9566983496e16b0864
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-26 02:55:52 +0000

    show,destroy link,server implement

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'about' => 'homes#about'
   resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:index, :show, :edit, :create]
+  resources :books, only: [:index, :show, :edit, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
